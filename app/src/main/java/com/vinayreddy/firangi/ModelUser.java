aa1c6d2bce2b0ffa8ef9f9091dbc915b1e4a2f48
sujay <ysujaykumar@gmail.com>
2021-02-28 22:29:09 -0500

    added the files for the project

---
+++ app/src/main/java/com/vinayreddy/firangi/ModelUser.java
@@ -0,0 +1,13 @@
+package com.vinayreddy.firangi;
+import com.google.firebase.database.IgnoreExtraProperties;
+@IgnoreExtraProperties
+public class ModelUser {
+    public String id;
+
+    public String Name;
+    public String Email;
+    public String Password;
+    public String PhoneNumber;
+
+
+}
